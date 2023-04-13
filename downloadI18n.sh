ba80664312018c66a8048619c841599de1e4bf65
lynxstirp <lynxstirp@gmail.com>
2023-04-14 01:32:51 +0800

    update

--- downloadI18n.sh
+++ downloadI18n.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 curl -s https://iwarship.net/wowsdb/index | grep I18N | grep zh-cn | grep -o '\"{.*}\"' | jq 'fromjson' > preset/i18n.json
+sed -i "s/DOCK_CONSUME_TITLE_//g" preset/i18n.json
